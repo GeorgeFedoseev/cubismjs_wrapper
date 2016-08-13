9163c9124d81078b326b9856a238b128d7a2bc6c
George Fedoseev <george.fedoseev@me.com>
2016-08-13 20:48:49 +0300

    added color options and start/end date

--- graph.js
+++ graph.js
@@ -5,8 +5,13 @@ class Graph {
     var default_options = {
       full_width: false,
       width: 500,
+      from_date: new Date(2016, 7, 13),
+      to_date: Date.now(),
       min_value: -1,
       max_value: 1,
+      negative_color: "#ff4d00",
+      positive_color: "#6eff6f",
+      show_palette: false,
       data_function: function(start, stop, step, callback) {
           var values = [];
           //console.log(start);
@@ -15,7 +20,7 @@ class Graph {
           stop = +stop;
           while (start < stop) {
               start += step;
-              values.push(Math.random());
+              values.push(Math.sin(start*0.01)/1);
           }
           callback(null, values);
       }
@@ -52,6 +57,45 @@ class Graph {
     return $.trim(this.options.name).replace(" ", "_");
   }
 
+  _horizon_colors(negative_color, positive_color){
+
+    var colors = [
+      ColorLuminance(negative_color, -0.5),
+      ColorLuminance(negative_color, -0.35),
+      ColorLuminance(negative_color, -0.2),
+      ColorLuminance(negative_color, 0.3),
+
+      ColorLuminance(positive_color, 0.3),
+      ColorLuminance(positive_color, -0.2),
+      ColorLuminance(positive_color, -0.35),
+      ColorLuminance(positive_color, -0.5)
+    ];
+
+    if(this.options.show_palette){
+      var palette = $("<div>");
+      this.graph_div.prepend(palette);
+      palette.html("");
+      // output color palette
+      for(var k in colors){
+        var c = colors[k];
+        var color_element = $("<div>");
+        color_element.css({
+          width: 10,
+          height: 10,
+          display: "inline-block",
+          "margin-right": 5,
+          "background": c
+        });
+
+        palette.append(color_element);
+      }
+    }
+
+    console.log("Palette: "+colors);
+
+    return colors
+  }
+
   _init(repeated){
     var _this = this;
 
@@ -62,14 +106,14 @@ class Graph {
       }
     }
 
-    var _size = this.options.full_width?$(window).width():this.options.width;
+    var _size = this.options.full_width?this.options.container.width():this.options.width;
 
-    var from_date = new Date(2013, 8, 1);
-    var to_date = Date.now();
+    var from_date = this.options.from_date;
+    var to_date = this.options.to_date;
     var _delay =  Date.now() - to_date;
     var _step = (to_date - from_date)/_size;
-    console.log(_delay);
-    console.log(new Date());
+    //console.log(_delay);
+  //  console.log(new Date());
 
     // create new cubism.js context to render
     var graphContext = cubism.context()
@@ -86,6 +130,7 @@ class Graph {
     // div and then we can create a chart
     var graphElement = document.createElement("div");
     $(graphElement).attr("id", this._graph_id());
+    this.graph_div = $(graphElement);
     this.options.container.append(graphElement);
     d3.select(graphElement).call(function(div) {
         div.append("div")
@@ -96,7 +141,10 @@ class Graph {
             .data([graphMetric])
           .enter().append("div")
             .attr("class", "horizon")
-            .call(graphContext.horizon().extent([_this.options.min_value, _this.options.max_value]));
+            .call(graphContext.horizon()
+                    .colors(_this._horizon_colors(_this.options.negative_color, _this.options.positive_color ))
+                    .extent([_this.options.min_value, _this.options.max_value])
+          );
 
         div.append("div")
             .attr("class", "rule")
@@ -113,4 +161,24 @@ class Graph {
     this._init(true);
   }
 
+}
+
+
+function ColorLuminance(hex, lum) {
+	// validate hex string
+	hex = String(hex).replace(/[^0-9a-f]/gi, '');
+	if (hex.length < 6) {
+		hex = hex[0]+hex[0]+hex[1]+hex[1]+hex[2]+hex[2];
+	}
+	lum = lum || 0;
+
+	// convert to decimal and change luminosity
+	var rgb = "#", c, i;
+	for (i = 0; i < 3; i++) {
+		c = parseInt(hex.substr(i*2,2), 16);
+		c = Math.round(Math.min(Math.max(0, c + (c * lum)), 255)).toString(16);
+		rgb += ("00"+c).substr(c.length);
+	}
+
+	return rgb;
 }
--- graphs_main.js
+++ graphs_main.js
@@ -3,7 +3,11 @@ $(function(){
     name: "Mr Graph",
     container: $("#graphs"),
     full_width: true,
-    max_value: 3
+    max_value: 0.2,
+    from_date: Date.parse("13 Jul 2016"),
+    to_date: Date.parse("13 Aug 2016"),
+    negative_color: "#4595ff",
+    show_palette: true
   });
 
 });
